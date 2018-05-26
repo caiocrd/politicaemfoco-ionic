ee92105b2a5ff4e89b1b35ffaade944721006f7b
Caio Cesar <caio.crd@gmail.com>
2018-05-26 12:18:59 -0300

    Adjusting tumb for radio and youtube videios

--- src/components/post-card/post-card.ts
+++ src/components/post-card/post-card.ts
@@ -1,7 +1,8 @@
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy} from '@angular/core';
 import { Content } from 'ionic-angular';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 
 import { IAuthorState, AppState } from './../../reducers';
 /*
@@ -23,15 +24,20 @@ export class PostCardComponent {
   categories: Array<any>;
   tags: Array<any>;
   author$: Observable<IAuthorState>;
-
+  featured_video: SafeHtml;
+  
   constructor(
-    private store: Store<AppState>,
+    private store: Store<AppState>, private sanitise: DomSanitizer
   ) { }
 
   ngOnInit() {
     const terms = this.post._embedded['https://api.w.org/term'] || this.post._embedded['wp:term'];
     this.categories = terms && terms[0];
     this.tags = terms && terms[1];
-    this.author$ = this.store.select(state => state.items.users && state.items.users[this.post.author])
+    this.author$ = this.store.select(state => state.items.users && state.items.users[this.post.author]);
+    
+    if(this.post.metadata._featured_video){
+      this.featured_video =  this.sanitise.bypassSecurityTrustHtml(this.post.metadata._featured_video[0].slice(34, this.post.metadata._featured_video[0].length - 3).replace("560", " 100%").replace("315", "200"));
+    }
   }
 }
